f77e0ef942f2dbdd070c2d9f1341c2c5543d587a
Vincent Toureau <vincent.toureau@grenoble-inp.org>
2019-07-25 11:43:56 +0200

    Ensure packaging

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="skin_sym_pkg",
+    include_package_data=True,
+    name="dermoscopic_symmetry",
     version="0.0.1",
     author="Vincent Toureau",
     author_email="Vincent.Toureau@grenoble-inp.org",
-    description="A package to study skin lesion's symmetry and help diagnose diseases like menalomas",
+    description="A package to study skin lesion's symmetry and help diagnose diseases like menalomas.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/GalakVince/skin_lesion_symmetry",
+    url="https://github.com/GalakVince/dermoscopic_symmetry",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
